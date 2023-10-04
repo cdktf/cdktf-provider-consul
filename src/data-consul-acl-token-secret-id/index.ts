f6ffaedec0becf644d30a1f9d90f85ec6aeeeb2f
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2023-10-04 14:14:51 +0000

    chore(deps): Updated  by cdktf/cdktf-repository-manager@20c94c5

--- src/data-consul-acl-token-secret-id/index.ts
+++ src/data-consul-acl-token-secret-id/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/consul/2.18.0/docs/data-sources/acl_token_secret_id
 // generated from terraform resource schema
 
