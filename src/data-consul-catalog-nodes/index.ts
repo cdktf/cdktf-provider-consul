0a26bcbb7c58b493d8ea033e703ce74ac7c17d7c
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-01-11 13:00:22 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@af3609a

--- src/data-consul-catalog-nodes/index.ts
+++ src/data-consul-catalog-nodes/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/consul/2.20.0/docs/data-sources/catalog_nodes
 // generated from terraform resource schema
 
